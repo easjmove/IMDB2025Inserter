fda7f602b1fd7ad5a0453570a28156b479a13b6b
move <move@zealand.dk>
2025-09-18 13:48:12 +0200

    added titletypes

--- IMDB2025Inserter/Program.cs
+++ IMDB2025Inserter/Program.cs
@@ -1,15 +1,66 @@
-﻿
+﻿using IMDB2025Inserter;
+using Microsoft.Data.SqlClient;
+
+string connectionString = "Server=localhost;Database=IMDB;" +
+    "integrated security=True;TrustServerCertificate=True;";
+SqlConnection sqlConn = new SqlConnection(connectionString);
+sqlConn.Open();
+SqlTransaction sqlTrans = sqlConn.BeginTransaction();
+
+Dictionary<string, int> TitleTypes = new Dictionary<string, int>();
+
 string filename = "c:/temp/title.basics.tsv";
 IEnumerable<string> imdbData = File.ReadAllLines(filename).Skip(1).Take(1000);
-foreach (string title in imdbData)
+foreach (string titleString in imdbData)
 {
-    string[] values = title.Split('\t');
+    string[] values = titleString.Split('\t');
     if (values.Length == 9)
     {
-        
+        if (!TitleTypes.ContainsKey(values[1]))
+        {
+            AddTitleType(values[1], sqlConn, sqlTrans, TitleTypes);
+        }
+
+        try
+        {
+            Title title = new Title
+            {
+                Id = int.Parse(values[0].Substring(2)),
+                TitleType = TitleTypes[values[1]],
+                PrimaryTitle = values[2],
+                OriginalTitle = values[3] == "\\N" ? null : values[3],
+                IsAdult = values[4] == "1",
+                StartYear = values[5] == "\\N" ? null : int.Parse(values[5]),
+                EndYear = values[6] == "\\N" ? null : int.Parse(values[6]),
+                RuntimeMinutes = values[7] == "\\N" ? null : int.Parse(values[7]),
+                Genres = values[8] == "\\N" ? new List<string>() : values[8].Split(',').ToList()
+            };
+
+            SqlCommand sqlComm = new SqlCommand("", sqlConn, sqlTrans);
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine("Error parsing line: " + titleString);
+            Console.WriteLine(ex.Message);
+        }
     }
     else
     {
-        Console.WriteLine("Not 9 values: " + title);
+        Console.WriteLine("Not 9 values: " + titleString);
+    }
+}
+
+sqlTrans.Rollback();
+sqlConn.Close();
+
+void AddTitleType(string titleType, SqlConnection sqlConn, SqlTransaction sqlTrans, Dictionary<string, int> TitleTypes)
+{
+    if (!TitleTypes.ContainsKey(titleType))
+    {
+        SqlCommand sqlComm = new SqlCommand(
+            "INSERT INTO TitleTypes (TitleType) VALUES (" + titleType + "); " +
+            "SELECT SCOPE_IDENTITY();", sqlConn, sqlTrans);
+        int newId = Convert.ToInt32(sqlComm.ExecuteScalar());
+        TitleTypes[titleType] = newId;
     }
 }
--- IMDB2025Inserter/Title.cs
+++ IMDB2025Inserter/Title.cs
@@ -9,13 +9,41 @@ namespace IMDB2025Inserter
     public class Title
     {
         public int Id { get; set; }
-        public string TitleType { get; set; }
+        public int TitleType { get; set; }
         public string PrimaryTitle { get; set; }
-        public string OriginalTitle { get; set; }
+        public string? OriginalTitle { get; set; }
         public bool IsAdult { get; set; }
-        public int StartYear { get; set; }
-        public int EndYear { get; set; }
-        public int RuntimeMinutes { get; set; }
+        public int? StartYear { get; set; }
+        public int? EndYear { get; set; }
+        public int? RuntimeMinutes { get; set; }
         public List<string> Genres { get; set; }
+
+        public Title()
+        {
+            Genres = new List<string>();
+        }
+
+        public string ToSQL()
+        {
+            StringBuilder sb = new StringBuilder();
+            sb.Append($"INSERT INTO Titles (Id, TitleType, PrimaryTitle, " +
+                $"OriginalTitle, IsAdult, StartYear, EndYear, RuntimeMinutes) " +
+                $"VALUES (");
+            sb.Append($"{Id}, ");
+            sb.Append($"{TitleType}, ");
+            sb.Append($"'{PrimaryTitle.Replace("'", "''")}', ");
+            sb.Append(OriginalTitle != null ? $"'{OriginalTitle.Replace("'", "''")}', " : "NULL, ");
+            sb.Append($"{(IsAdult ? 1 : 0)}, ");
+            sb.Append(StartYear.HasValue ? $"{StartYear.Value}, " : "NULL, ");
+            sb.Append(EndYear.HasValue ? $"{EndYear.Value}, " : "NULL, ");
+            sb.Append(RuntimeMinutes.HasValue ? $"{RuntimeMinutes.Value}" : "NULL");
+            sb.Append(");");
+            //foreach (string genre in Genres)
+            //{
+            //    sb.AppendLine();
+            //    sb.Append($"INSERT INTO TitleGenres (TitleId, Genre) VALUES ({Id}, '{genre.Replace("'", "''")}');");
+            //}
+            return sb.ToString();
+        }
     }
 }
