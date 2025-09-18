3627a50c348279c5c80dfe269b05d28804b339f5
move <move@zealand.dk>
2025-09-18 13:05:50 +0200

    Add project files.

---
+++ IMDB2025Inserter/Program.cs
@@ -0,0 +1,15 @@
+﻿
+string filename = "c:/temp/title.basics.tsv";
+IEnumerable<string> imdbData = File.ReadAllLines(filename).Skip(1).Take(1000);
+foreach (string title in imdbData)
+{
+    string[] values = title.Split('\t');
+    if (values.Length == 9)
+    {
+        
+    }
+    else
+    {
+        Console.WriteLine("Not 9 values: " + title);
+    }
+}
